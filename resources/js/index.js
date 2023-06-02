8933944a7fbb08daf58bccef5dc266248152cbba
shandrii83 <sharmarandrey@gmail.com>
2023-06-02 15:55:43 +0200

    Pagina principal sin fetch y no ajustado para mobiles

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,30 @@
-/* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+/*  Creamos la lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation  */
+const newCampo1 = document.getElementById('campo1');
+const url = 'https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects';
+function NewElement(element) {
+    return document.createElement(element);
+
+}
+function append(parent, el) {
+return parent.appendChild(el);
+}
+fetch(url)
+.then((resp) => resp.json())
+.then(function (data) {  
+    let campoOne = data.results;
+    return campoOne.map(function(campo){
+        console.log(campo);
+let h3 = createNode("h3");
+let p = createNode ("p");
+append(h3, "name");
+append(p, "description" );
+
+    })
+  })
+  .catch(function(error) {
+    console.log(error);
+    
+  });
+
+
+
