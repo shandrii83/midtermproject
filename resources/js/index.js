1ea0ed4e0e896d084682a44014adcc9dbb8528a1
shandrii83 <sharmarandrey@gmail.com>
2023-06-07 14:32:50 +0200

    Proecto con paginas index.html, progects y contct Us

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,29 +1,50 @@
-/*  Creamos la lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation  */
-const Campo = document.getElementById('campo1');
-const url = 'https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects';
-function NewElement(element) {
-    return document.createElement(element);
+fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const projects = document.getElementsByClassName("project");
+    const randomIndexes = generateRandomIndexes(data.length);
+
+    Array.from(projects).forEach((project, index) => {
+      const randomIndex = randomIndexes[index];
+      const randomData = data[randomIndex];
+
+      project.innerHTML = `
+        <img src="${randomData.image}" alt="">
+        <h3>${randomData.name}</h3>
+        <p>${randomData.description}</p>
+        <a href="#">Learn More</a>
+      `;
+    });
+  });
+
+function generateRandomIndexes(length) {
+  const indexes = [];
+  for (let i = 0; i < length; i++) {
+    indexes.push(i);
+  }
+  return shuffleArray(indexes);
 }
 
-/* function append(parent, el) {
-return parent.appendChild(el);
-} */
-fetch(url)
-.then((resp) => resp.json())
-.then(data => {
-  // Anadimos los datos al elemento
-  document.getElementById('campo1').innerHTML =data.name;
-});
-console.log(data);
-
-
-/* var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("scroolToTop").style.display = "block";
-  } else {
-    document.getElementById("scroolToTop").style.display = "none";
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-  prevScrollpos = currentScrollPos;
-} */
+  return array;
+}
+
+
+
+window.addEventListener('scroll', function () {
+  var scrollToTopButton = document.getElementById('scrollToTop');
+
+  if (window.pageYOffset > 0) {
+    scrollToTopButton.classList.remove('hidden');
+  } else {
+    scrollToTopButton.classList.add('hidden');
+  }
+});
+
+
